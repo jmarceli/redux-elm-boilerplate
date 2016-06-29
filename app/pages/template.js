9ce51b263fc1d375aec683dfe00e600ed1fd621b
Jan Grzegorowski <grzegorowski@gmail.com>
2016-06-29 22:48:08 +0200

    feat: add react-bootstrap navbar

--- app/pages/template.js
+++ app/pages/template.js
@@ -13,17 +13,20 @@ import styles from './styles.css';
 export default class Template extends React.Component {
   render() {
     return (
-      <div>
+      <div style={{ paddingTop: 50 }}>
         <Navbar fixedTop className={styles.Navbar}>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link href="/">Redux-elm-boilerplate</Link>
+              <Link to="/">Redux-elm-boilerplate</Link>
             </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="/home">Home</NavItem>
-            <NavItem eventKey={2} href="/home2">Home 2</NavItem>
-          </Nav>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="/home">Home</NavItem>
+              <NavItem eventKey={2} href="/home2">Home 2</NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <main>
           {this.props.children}
