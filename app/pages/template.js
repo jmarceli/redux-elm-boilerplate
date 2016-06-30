59ce2a54889de6846ad74bdfc17fc662dfa4c25e
Jan Grzegorowski <grzegorowski@gmail.com>
2016-06-30 14:12:26 +0200

    refactor: use stateless function for Template component

--- app/pages/template.js
+++ app/pages/template.js
@@ -5,27 +5,22 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-// TODO: maybe there is some better implementation
-// e.g. using stateless functional components
-// eslint-disable-next-line react/prefer-stateless-function
-export default class Template extends React.Component {
-  render() {
-    return (
-      <section style={{ paddingTop: 50 }}>
-        <header>
-          <Navbar />
-        </header>
-        <main>
-          {this.props.children}
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </section>
-    );
-  }
-}
+const Template = (props) => (
+  <section style={{ paddingTop: 50 }}>
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      {props.children}
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+  </section>
+);
 
 Template.propTypes = {
   children: React.PropTypes.object,
 };
+
+export default Template;
